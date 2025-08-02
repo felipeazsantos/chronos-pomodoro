7f00f1a0a9dc2d739876867fd2d294655997a81f
felipeazsantos <felipeasantos10@hotmail.com>
2025-08-02 18:36:05 -0300

    creating web worker for countdown

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,7 +1,9 @@
-import { useReducer } from 'react';
+import { useEffect, useReducer } from 'react';
 import { TaskContext } from './TaskContext';
 import { initialTaskState } from './initialTaskState';
 import { taskReducer } from './taskReducer';
+import { TimerWorkerManager } from '../../workers/TimerWorkerManager';
+import { TaskActionTypes } from './taskActions';
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -9,6 +11,35 @@ type TaskContextProviderProps = {
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, dispatch] = useReducer(taskReducer, initialTaskState);
+
+  const worker = TimerWorkerManager.getInstance();
+
+  worker.onmessage(e => {
+    console.log(e.data);
+
+    const countDownSecods = e.data;
+
+    if (countDownSecods <= 0) {
+      dispatch({
+        type: TaskActionTypes.COMPLETE_TASK,
+      });
+
+      worker.terminate();
+    } else {
+      dispatch({
+        type: TaskActionTypes.COUNT_DOWN,
+        payload: { secondsRemaining: countDownSecods },
+      });
+    }
+  });
+
+  useEffect(() => {
+    if (!state.activeTask) {
+      worker.terminate();
+    }
+
+    worker.postMessage(state);
+  }, [worker, state]);
 
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
