691de6f22f625005a63d5c6662556e2d1b045b0f
felipeazsantos <felipeasantos10@hotmail.com>
2025-08-03 15:32:55 -0300

    applying adapter pattern to react toastify

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -9,6 +9,7 @@ import { getNextCycle } from '../../utils/getNextCycle';
 import { getNextCycleType } from '../../utils/getNextCycleType';
 import { TaskActionTypes } from '../../contexts/TaskContext/taskActions';
 import { Tips } from '../Tips';
+import { showMessage } from '../../adapters/showMessage';
 
 export function MainForm() {
   const { state, dispatch } = useTaskContext();
@@ -19,12 +20,13 @@ export function MainForm() {
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+    showMessage.dismiss();
 
     if (taskNameInput.current == null) return;
 
     const taskName = taskNameInput.current.value.trim();
     if (!taskName) {
-      alert('digite o nome da tarefa');
+      showMessage.warn('Digite o nome da tarefa');
       return;
     }
 
@@ -39,6 +41,8 @@ export function MainForm() {
     };
 
     dispatch({ type: TaskActionTypes.START_TASK, payload: newTask });
+
+    showMessage.success('Tarefa iniciada');
   }
 
   function handleInterruptTask(
@@ -49,6 +53,9 @@ export function MainForm() {
     dispatch({
       type: TaskActionTypes.INTERRUPT_TASK,
     });
+
+    showMessage.dismiss();
+    showMessage.error('Tarefa interrompida');
   }
 
   return (
