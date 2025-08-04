8914c8c163fbb0417ab480f16b2f6268f8c95df9
felipeazsantos <felipeasantos10@hotmail.com>
2025-08-04 12:26:25 -0300

    populating table with state tasks

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -5,8 +5,11 @@ import { Heading } from '../../components/Heading';
 import { MainTemplate } from '../../templates/MainTemplate';
 
 import styles from './styles.module.css';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 
 export function History() {
+  const { state } = useTaskContext();
+
   return (
     <MainTemplate>
       <Container>
@@ -36,13 +39,17 @@ export function History() {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>Estudar</td>
-                <td>25min</td>
-                <td>04/08/2025 08:00</td>
-                <td>Completa</td>
-                <td>Foco</td>
-              </tr>
+              {state.tasks.map(task => {
+                return (
+                  <tr key={task.id}>
+                    <td>{task.name}</td>
+                    <td>{task.duration}min</td>
+                    <td>{new Date(task.startDate).toISOString()}</td>
+                    <td>{task.completeDate ? 'COMPLETED' : ''}</td>
+                    <td>{task.type}</td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
         </div>
