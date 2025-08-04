62df97ad6f2ee57a6d041583c8bdf7ba4549b69f
felipeazsantos <felipeasantos10@hotmail.com>
2025-08-04 12:41:26 -0300

    format Date and task status

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -6,6 +6,8 @@ import { MainTemplate } from '../../templates/MainTemplate';
 
 import styles from './styles.module.css';
 import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import { formatDate } from '../../utils/formatDate';
+import { getTaskStatus } from '../../utils/getTaskStatus';
 
 export function History() {
   const { state } = useTaskContext();
@@ -44,8 +46,8 @@ export function History() {
                   <tr key={task.id}>
                     <td>{task.name}</td>
                     <td>{task.duration}min</td>
-                    <td>{new Date(task.startDate).toISOString()}</td>
-                    <td>{task.completeDate ? 'COMPLETED' : ''}</td>
+                    <td>{formatDate(task.startDate)}</td>
+                    <td>{getTaskStatus(task, state.activeTask)}</td>
                     <td>{task.type}</td>
                   </tr>
                 );
